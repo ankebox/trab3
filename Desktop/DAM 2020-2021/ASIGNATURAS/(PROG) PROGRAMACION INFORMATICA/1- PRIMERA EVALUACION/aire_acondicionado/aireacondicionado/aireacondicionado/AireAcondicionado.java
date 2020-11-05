2c7e9192d8ada5c977b6d7b7c1669abffa012e51
David <davidaicardocossiopulido@gmail.com>
2020-11-05 23:13:26 +0100

    0497.02 creada la estructura basica de la clase

--- Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/aire_acondicionado/aireacondicionado/aireacondicionado/AireAcondicionado.java
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/aire_acondicionado/aireacondicionado/aireacondicionado/AireAcondicionado.java
@@ -7,27 +7,24 @@
  */
 public class AireAcondicionado
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class AireAcondicionado
-     */
+   
+    private double temperatura;
+    
+    
     public AireAcondicionado()
     {
-        // initialise instance variables
-        x = 0;
+        temperatura = 15.0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void subirTemperatura(){
+        temperatura = temperatura + 5.0;
+    }
+    
+    public void bajarTemperatura(){
+        temperatura = temperatura - 5.0;
+    }
+    
+    public double getTemperatura(){
+        return temperatura;
     }
 }
