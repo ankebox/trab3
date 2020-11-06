fc279d111a92b59aa0c5c75dae88df46ec882eef
David <davidaicardocossiopulido@gmail.com>
2020-11-06 12:09:38 +0100

    hg

--- Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/calendario/micale/cale/cale.java
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/calendario/micale/cale/cale.java
@@ -2,19 +2,43 @@
 public class cale
 {
     
-    private int x;
-
+    private int dia;
+    private int mes;
+    private int año;
+    
    
     public cale()
     {
-     
-        x = 0;
+        dia = 1;
+        mes = 1;
+        año = 1;
     }
-
-   
-    public int sampleMethod(int y)
-    {
-       
-        return x + y;
+    
+    public String obtenerFecha(){
+        return (dia + " - " + mes + " - " + año);
     }
+    
+    public void fijarFecha(int diaDe, int mesDe, int añoDe){
+        dia = diaDe;
+        mes = mesDe;
+        año = añoDe;
+    }
+    
+    public void avanzarFecha(){
+        dia = dia + 1;
+        if(dia == 31){
+            mes = mes +1;
+            dia = 1;
+            if(mes == 13){
+                    año = año + 1;
+                    mes = 1;
+                    if(año == 100){
+                        año = 1;
+                    }
+            }
+        }
+        
+        
+    }
+    
 }
