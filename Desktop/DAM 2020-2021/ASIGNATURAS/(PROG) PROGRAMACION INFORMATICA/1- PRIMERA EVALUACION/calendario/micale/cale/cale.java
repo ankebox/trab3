32f906dc7841fe7272d562c9007d44e3b39516a5
David <davidaicardocossiopulido@gmail.com>
2020-11-06 11:07:50 +0100

    0321.01

---
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/calendario/micale/cale/cale.java
@@ -0,0 +1,20 @@
+
+public class cale
+{
+    
+    private int x;
+
+   
+    public cale()
+    {
+     
+        x = 0;
+    }
+
+   
+    public int sampleMethod(int y)
+    {
+       
+        return x + y;
+    }
+}
