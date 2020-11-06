8e196ec45b8dd349ba1232d74e772a656085a24e
David <davidaicardocossiopulido@gmail.com>
2020-11-06 10:10:18 +0100

    obteniendo estadisticas del aparato

--- Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/aire_acondicionado/aireacondicionado/aireacondicionado/AireAcondicionado.java
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/aire_acondicionado/aireacondicionado/aireacondicionado/AireAcondicionado.java
@@ -12,24 +12,27 @@ public class AireAcondicionado
     private double min;
     private double max;
     private double incremento;
-    
+    private int cambioTemperatura;
     public AireAcondicionado(double minDe, double maxDe)
     {
         temperatura = 15.0;
         incremento = 5.0;
         min = minDe;
         max = maxDe;
+        cambioTemperatura = 0;
     }
     
     public void subirTemperatura(){
         if(temperatura + incremento <= max){
             temperatura = temperatura + incremento;
+            cambioTemperatura = cambioTemperatura + 1;
         }   
     }
     
     public void bajarTemperatura(){
         if(temperatura - incremento >= min){
             temperatura = temperatura - incremento;
+            cambioTemperatura = cambioTemperatura + 1;
         }
     }
     
@@ -41,6 +44,14 @@ public class AireAcondicionado
         if(incrementoDe > 0){
             incremento = incrementoDe;
         }
+    }
+    
+    public void mostrarEstadisticas(){
+        System. out. println("Temperatura actual: " + temperatura + " temperatura minima seleccionada: " + min + " Temperatura maxima seleccionada: " + max + " Temperatura Cambiada " + cambioTemperatura + " veces." );
+    }
+    
+    public String getEstadisticas(){
+        return ("Temperatura actual: " + temperatura + " temperatura minima seleccionada: " + min + " Temperatura maxima seleccionada: " + max + " Temperatura Cambiada " + cambioTemperatura + " veces." );
     }
 }
 
