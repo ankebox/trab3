96a56e6cb84dd6852cde07e77e46cad6b2c15048
David <davidaicardocossiopulido@gmail.com>
2020-11-18 09:36:32 +0100

    02

---
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/relog mas complejo/reloj/PantallaHoraYFecha.java
@@ -0,0 +1,16 @@
+
+
+public class PantallaHoraYFecha
+{
+    
+    private String desecho;
+
+    
+    public PantallaHoraYFecha()
+    {
+        
+       String desecho = " 1 de enero del año 1 a las 00:00";
+    }
+
+    
+}
