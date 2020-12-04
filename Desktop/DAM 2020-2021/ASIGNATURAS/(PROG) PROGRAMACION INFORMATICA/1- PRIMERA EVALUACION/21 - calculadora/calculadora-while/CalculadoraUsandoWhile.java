76dc8f4f10902da7b12f3fa277ab84c47c4061c7
David <davidaicardocossiopulido@gmail.com>
2020-12-04 09:09:25 +0100

    calculadora multiplos de cinco

---
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/1- PRIMERA EVALUACION/21 - calculadora/calculadora-while/CalculadoraUsandoWhile.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class CalculadoraUsandoWhile here.
+ * 
+ * @author (ankebox) 
+ * @version (1.00)
+ */
+public class CalculadoraUsandoWhile
+{
+    
+    
+
+    /**
+     * Constructor for objects of class CalculadoraUsandoWhile
+     */
+    public CalculadoraUsandoWhile()
+    {
+       
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void multiplosDeCinco()
+    {
+       int refer = 15;
+       while (refer <= 90) {
+           System. out. println(refer);
+           refer = refer + 5;
+        }
+    }
+}
