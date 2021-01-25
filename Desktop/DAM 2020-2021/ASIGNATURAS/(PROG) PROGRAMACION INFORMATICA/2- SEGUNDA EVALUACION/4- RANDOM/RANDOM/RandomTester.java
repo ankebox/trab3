da1b288ff0651d97d6052ff211677189a2b8eae6
David <davidaicardocossiopulido@gmail.com>
2021-01-25 16:23:41 +0100

    ampliando la clase ramdon tester

--- Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/2- SEGUNDA EVALUACION/4- RANDOM/RANDOM/RandomTester.java
+++ Desktop/DAM 2020-2021/ASIGNATURAS/(PROG) PROGRAMACION INFORMATICA/2- SEGUNDA EVALUACION/4- RANDOM/RANDOM/RandomTester.java
@@ -1,3 +1,4 @@
+
 import java.util.Random;
 public class RandomTester
 {
@@ -26,6 +27,44 @@ public class RandomTester
             System.out.println(numeroGenerado);
             contador = contador + 1;
         }
-
+    }
+    
+    public void lanzarDado() {
+        Random generador = new Random();
+        int numeroAleatorio = generador.nextInt(6);
+        numeroAleatorio = numeroAleatorio + 1;
+        System.out.println(numeroAleatorio);
+    }
+    
+    public void getRespuesta() {
+        Random generador = new Random();
+        int numeroAleatorio = generador.nextInt(3);
+        if (numeroAleatorio == 0) {
+            System.out.println("si");
+        }
+        else {
+            if (numeroAleatorio == 1) {
+                System.out.println("no");
+            }
+            else {
+                System.out.println("quizas");
+            }
+        }
+    }
+    
+    public void getNumeroEntre0Y (int deMaximo) {
+        Random generador = new Random();
+        int maximo = deMaximo;
+        int numeroAleatorio = generador.nextInt(maximo);
+        numeroAleatorio = numeroAleatorio + 1;
+        System.out.println(numeroAleatorio);
+    }
+    
+    public void getNumeroEntre (int deMinimo, int deMaximo) {
+        Random generador = new Random();
+        int maximo =  deMaximo;
+        int minimo = deMinimo;
+        int numeroAleatorio = minimo + generador.nextInt((maximo + 1) - minimo);
+        System.out.println(numeroAleatorio);
     }
 }
